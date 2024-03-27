3c9c6f3d40d3337ac988c1a06a53c4e659ee3d41
jayson-ambrose <jayson.ambrose.87@gmail.com>
2024-03-26 22:02:57 -0400

    added db.json with some mock data

--- app/ui/browse/pet-card.tsx
+++ app/ui/browse/pet-card.tsx
@@ -5,7 +5,6 @@ export default function PetCard () {
 
     return (
         <div className="card">
-            <h1>hello world</h1>
             <img className="card-img" src="/Card 1.svg" alt="Card1" />
         </div>
     )
