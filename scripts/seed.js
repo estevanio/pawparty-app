24cae2d4082c790ce276f2b50cbbd18f20404395
stavinopolis <estevanbenjamin@gmail.com>
2024-04-16 16:36:39 -0600

    Database seeded!

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,35 +5,8 @@ const attributes = require('./mock-attributes.json');
 const shelters = require('./mock-shelters.json');
 const photos = require('./mock-photos.json');
 
-const bcrypt = require('bcrypt');
-
 const prisma = new PrismaClient();
 
-// async function seedUsers() {
-//   try {
-//     // Insert data into the "users" table
-//     const insertedUsers = await Promise.all(
-//       users.map(async (user) => {
-//         const hashedPassword = await bcrypt.hash(user.password, 10);
-//         return prisma.user.create({
-//           data: {
-//             id: user.id,
-//             name: user.name,
-//             email: user.email,
-//             password: hashedPassword,
-//           },
-//         });
-//       }),
-//     );
-
-//     console.log(`Seeded ${insertedUsers.length} users`);
-
-//     return insertedUsers;
-//   } catch (error) {
-//     console.error('Error seeding users:', error);
-//     throw error;
-//   }
-// }
 
 async function seedAnimals() {
   try {
@@ -41,10 +14,15 @@ async function seedAnimals() {
     const insertedAnimals = await Promise.all(
       animals.map((animal) => prisma.animal.create({
         data: {
-          id: animal.id,
+          animal_id: animal.uniqueId,
           name: animal.name,
           species: animal.species,
-          shelter_id: animal.shelter_id,
+          breed: animal.breed,
+          secondary_breed: animal.secondary_breed,
+          primary_color: animal.primary_color,
+          secondary_color: animal.secondary_color,
+          intake_date: new Date(animal.intake_date),
+          shelter_id: "43ff709b-2c6d-4087-8179-b61b87bfe36e"
         },
       })),
     );
@@ -86,9 +64,30 @@ async function seedShelters() {
     const insertedShelters = await Promise.all(
       shelters.map((shelter) => prisma.shelter.create({
         data: {
-          id: shelter.id,
           name: shelter.name,
           location: shelter.location,
+          location: shelter.location,
+          phone_number: shelter.phone_number,
+          email: shelter.email,
+          website_url: shelter.website_url,
+          opening_hours: shelter.opening_hours,
+          days_of_operation: shelter.days_of_operation,
+          cat_facilities: shelter.cat_facilities,
+          dog_facilities: shelter.dog_facilities,
+          volunteer_opportunities: shelter.volunteer_opportunities,
+          volunteer_coordinator_contact: shelter.volunteer_coordinator_contact,
+          adoption_process_details: shelter.adoption_process_details,
+          adoption_fees: shelter.adoption_fees,
+          veterinary_services: shelter.veterinary_services,
+          exercise_programs: shelter.exercise_programs,
+          educational_programs: shelter.educational_programs,
+          community_outreach: shelter.community_outreach,
+          donation_methods: shelter.donation_methods,
+          donation_collection_points: shelter.donation_collection_points,
+          donation_needs: shelter.donation_needs,
+          licensing_details: shelter.licensing_details,
+          shelter_history: shelter.shelter_history,
+          mission_statement: shelter.mission_statement,
         },
       })),
     );
@@ -105,17 +104,16 @@ async function seedShelters() {
 async function seedPhotos() {
   try {
     // Insert data into the "photos" table
-    const insertedPhotos = await Promise.all(
-      photos.map((photo) => prisma.photo.create({
-        data: {
-          id: photo.id,
-          url: photo.url,
-          animal_id: photo.animal_id,
-        },
-      })),
-    );
+    const insertedPhotos = await prisma.photo.createMany({ data: photos.map((photo) => ({
+        photo_id: photo.id,
+        url: photo.url,
+        name: photo.name,
+        animal_id: photo.animal_id,
+        is_cover: photo.is_cover,
+      }))
+    });
 
-    console.log(`Seeded ${insertedPhotos.length} photos`);
+    console.log(`Seeded ${insertedPhotos.count} photos`);
 
     return insertedPhotos;
   } catch (error) {
@@ -128,9 +126,9 @@ async function seedPhotos() {
 async function main() {
   await seedShelters();
   await seedAnimals();
-  await seedAttributes();
-  await seedShelters();
   await seedPhotos();
+  await seedAttributes();
+
   await prisma.$disconnect();
 }
 
