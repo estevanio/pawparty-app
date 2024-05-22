42a319862ba86098fa1d1dff6366ee9a600d744c
AlbertoPerez9001 <properezalberto8@gmail.com>
2024-05-21 19:40:25 -0700

    completed Sponsors section

--- app/ui/website/index.js
+++ app/ui/website/index.js
@@ -3,3 +3,4 @@ export { default as Folio } from './Folio';
 export { default as Gallery } from './Gallery';
 export { default as Hero } from './Hero';
 export { default as Services } from './Services';
+export { default as Sponsors } from './Sponsors';
