20b58bbffc08599d9e3ec1d0b9961cb4ee5c46c6
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-05-23 17:39:18 -0600

    fixing commit for dialogue branch sprint-03

---
+++ app/ui/browse/browse-questions-dialogue.tsx
@@ -0,0 +1,38 @@
+'use client'
+import React, { useState } from "react"
+import { Dialog, Container, Button, Typography } from "@mui/material"
+
+interface DialogProps {
+    questionsOpen: boolean,
+    setQuestionsOpen: Function
+}
+export default function BrowseQuestionsDialogue ({questionsOpen, setQuestionsOpen}: DialogProps) {
+    
+    function handleButtonPress(value: string) {
+        localStorage.setItem('questionsAnswered', JSON.stringify(true))
+        localStorage.setItem('questionAnswer', value)
+        setQuestionsOpen(false)
+    }
+
+    return (
+        <Dialog onClose={() => setQuestionsOpen(false)} open={questionsOpen}>
+            <Container sx={{
+                display: 'flex', 
+                flexDirection: 'column', 
+                alignItems:'center', 
+                height: 400, 
+                width: '90vw', 
+                borderRadius: '25px'}}>
+                <Typography sx={{fontFamily: 'Montserrat', fontSize: 16}}>What species of friend are you looking for?</Typography>
+                <Button 
+                    sx={{width: 100, marginTop:2}}
+                    variant='contained' 
+                    onClick={() => handleButtonPress('cat')}>Cats</Button>
+                <Button 
+                    sx={{width: 100, marginTop:2}}
+                    variant='contained' 
+                    onClick={() => handleButtonPress('dog')}>Dogs</Button>
+            </Container>
+        </Dialog>        
+    )
+}
--- app/ui/browse/swipe-stack.tsx
+++ app/ui/browse/swipe-stack.tsx
@@ -1,36 +1,55 @@
 'use client'
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TinderCard from 'react-tinder-card'
 import PetCard from "./pet-card";
 import { Card, CardContent, CardMedia, Typography, Container } from "@mui/material";
 import { pets } from '@/app/lib/mockdb'
 import { uptime } from "process";
-import { Pet } from "@/app/lib/definitions";
+import BrowseQuestionsDialogue from "./browse-questions-dialogue";
 
 export default function SwipeStack () {
     
     const [lastDirection, setLastDirection] = useState('')
+    const [swipeInProgress, setSwipeInProgress] = useState(false)
+    const [questionsOpen, setQuestionsOpen] = useState(false)
+
+    const browserStorage = localStorage
+
+    useEffect(() => {
+      browserStorage.getItem('questionsAnswered') ? null : setQuestionsOpen(true)
+    }, [])
   
-    const swiped: any = (direction: string, nameToDelete: string) => {
-      console.log('removing: ' + nameToDelete)
+    const swiped: any = (direction: string, name: string, breed: string[]) => {
+
+      //logs may be useful while building match feature.
+
+      // console.log('----------------------')
+      // console.log('swiped name: ' + name)
+      // console.log('swiped species: ' + breed[0])
+      // console.log(browserStorage.getItem('questionAnswer'))
+      // console.log('match: ' + (breed[0] == browserStorage.getItem('questionAnswer')))
+      // console.log('----------------------')
+
       setLastDirection(direction)
+      setSwipeInProgress(true)
     }
   
     const outOfFrame = (name: string) => {
       console.log(name + ' left the screen!')
+      setSwipeInProgress(false)
     }
 
-    const displayPets = pets.map((pet: Pet) => {        
+    const displayPets = pets.map((pet) => {        
         
         return(
             <TinderCard
                 className="swipe"
                 key={pet.id} 
-                onSwipe={(dir) => swiped(dir, pet.name)} 
+                onSwipe={(dir) => swiped(dir, pet.name, pet.breed)} 
                 onCardLeftScreen={() => outOfFrame(pet.name)}
                 preventSwipe={['up', 'down']}>
-                <PetCard pet={pet}/>
+                <PetCard pet={pet} swipeInProgress={swipeInProgress}/>
             </TinderCard>
         )
     })
@@ -40,6 +59,9 @@ export default function SwipeStack () {
         <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height:475, width: '100vw'}}>
           {displayPets}
         </Container>
+        <BrowseQuestionsDialogue 
+        questionsOpen={questionsOpen} 
+        setQuestionsOpen={setQuestionsOpen}/>
         {/* {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />} */}
       </>
     )
