15000c6143bd2f17b9cd31a2f8f8d25e0d743a81
jbmcmath <jbmcmath@ualr.edu>
2024-05-24 11:44:44 -0500

    Creation of "How It Works" page
    created "howitworks" folder with a page.tsx file. Added an empty section within.

---
+++ app/(website)/howitworks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HowItWorks: React.FC = () => {
+    return (
+    <section id="how-it-works">
+        {/*fill with content*/}
+    </section>
+    );
+};
+
+export default HowItWorks;
