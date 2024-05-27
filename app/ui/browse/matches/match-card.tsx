e8eadebfc354e8ad7ba3dcb73b66e5f0798a06ba
jayson-ambrose <jayson.ambrose.87@gmail.com>
2024-05-27 16:22:17 -0400

    building out match cards according to figma designs

--- app/ui/browse/matches/match-card.tsx
+++ app/ui/browse/matches/match-card.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from "react";
 import { 
-    Card, Container
+    Card, Container, Avatar
 } from "@mui/material";
 import { Pet } from "@/app/lib/definitions";
 
@@ -9,13 +9,22 @@ export default function MatchCard() {
 
     return(
       <Card
-        sx={{display: 'block', background: '#fff'}}>
-          <img className="matches-icon" src="dino-icon.svg" />
-          <div className="matches-card-text">
+        sx={{
+          display: 'flex', 
+          background: '#ededed',
+          borderRadius: '28px',
+          width: {
+            sm: '382px',
+            md: '90%',
+            lg: '90%'
+          },
+          height: '81px'}}>
+          <Avatar className="matches-icon" src="dino-icon.svg" />
+          <Container className="matches-card-text">
             <h2>Dino</h2>
             <h3 className="matches-type">German Shepherd Mix</h3>
             <p className="matches-location">Placentia, CA</p>
-          </div>
+          </Container>
       </Card>
     )
 }
