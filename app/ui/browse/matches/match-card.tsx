1defc627e1bf22584f9f440b008af3b02dc8e42e
jayson-ambrose <jayson.ambrose.87@gmail.com>
2024-05-27 22:53:00 -0400

    matches styled based on figma designs

--- app/ui/browse/matches/match-card.tsx
+++ app/ui/browse/matches/match-card.tsx
@@ -1,30 +1,56 @@
 'use client'
 import React from "react";
 import { 
-    Card, Container, Avatar
+    Card, Container, Avatar, Typography
 } from "@mui/material";
 import { Pet } from "@/app/lib/definitions";
 
 export default function MatchCard() {
 
+    const handleClick = () => {
+      console.log('match clicked')
+    }
+
     return(
       <Card
-        sx={{
-          display: 'flex', 
-          background: '#ededed',
-          borderRadius: '28px',
-          width: {
-            sm: '382px',
-            md: '90%',
-            lg: '90%'
-          },
-          height: '81px'}}>
-          <Avatar className="matches-icon" src="dino-icon.svg" />
-          <Container className="matches-card-text">
-            <h2>Dino</h2>
-            <h3 className="matches-type">German Shepherd Mix</h3>
-            <p className="matches-location">Placentia, CA</p>
+        sx={styles.card}
+        onClick={handleClick}>
+          <Avatar sx={styles.avatar}/>
+          <Container>
+            <Typography sx={styles.largeText}>Dino</Typography>
+            <Typography sx={styles.smallText}>German Shepherd Mix</Typography>
+            <Typography sx={styles.smallText}>Placentia, CA</Typography>
           </Container>
       </Card>
     )
+}
+
+const styles = {
+  card: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: '#ededed',
+    borderRadius: '28px',
+    width: {
+      sm: '382px',
+      md: '90%',
+      lg: '90%'
+    },
+    height: '81px'},
+  avatar: { 
+      marginLeft: '10px', 
+      height: '60px', 
+      width: '60px'},
+  largeText: { 
+    fontFamily: 'Montserrat', 
+    fontWeight: 700, 
+    fontSize: 18, 
+    lineHeight: '22px'},
+  smallText: {
+    fontFamily: 'Montserrat',
+    fontWeight: 400,
+    fontSize: 12,
+    lineHeight: '15px'
+  }
 }
