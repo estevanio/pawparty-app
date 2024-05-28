304b1351d7c91f6c83cb2a06682e43db11d7bb1a
BlankZ3ro <jbmcmath2@gmail.com>
2024-05-28 15:29:40 -0500

    fixed ""

--- app/ui/website/HowItWorks/HowItWorks.tsx
+++ app/ui/website/HowItWorks/HowItWorks.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const HowItWorks: React.FC = () => {
     return (
-    <section id="how-it-works">
-        <p>This is the "How-it-works section</p>
+    <section id='how-it-works'>
+        <p>This is the How-it-works section</p>
     </section>
     );
 };
