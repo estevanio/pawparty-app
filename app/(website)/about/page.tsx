cdd7c8c927da9b734918c74eb27466f6100eb79c
jbmcmath <jbmcmath@ualr.edu>
2024-05-30 15:21:00 -0500

    Created about page
    Added "about" folder with a page.tsx. The page.tsx includes a section with the id "ourMission" including some filler text.

---
+++ app/(website)/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+return(
+    <section id="ourMission">
+        <p>This is where the Full Mission/Vision should be</p>
+    </section>
+    );
+}
+
+export default About;
