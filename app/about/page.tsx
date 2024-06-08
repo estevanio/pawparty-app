4b8087f8710cd0fe3238ef6ceb2d6d8a7ad1f809
AlbertoPerez9001 <properezalberto8@gmail.com>
2024-06-08 13:50:50 -0700

    updated formats

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,6 +7,12 @@ export default function Page(){
     <>
         <Topbar />
         About us
+        <section id="Bios">
+            <p>This is where the Bios should be</p>
+        </section>
+        <section id="ourMission">
+            <p>This is where the Full Mission/Vision should be</p>
+        </section>
     </>
 );
 }
