e8d0b8bd5324bc02bcb2266d9718c9d5626fbfe0
jbmcmath <jbmcmath2@gmail.com>
2024-06-21 17:19:07 -0500

    Imported WithSimpleCards.js
    Imported WithSimpleCards.js into the website, adding it to the /about page.

--- app/ui/website/index.js
+++ app/ui/website/index.js
@@ -6,4 +6,5 @@ export { default as Services } from './Services';
 export { default as HowItWorks} from './HowItWorks';
 export { default as Sponsors } from './Sponsors';
 export { default as Topbar } from './layouts/Navigation/Topbar/Topbar';
-export { default as Faq } from './layouts/Faq'
+export { default as Faq } from './layouts/Faq';
+export {default as AboutDefault} from '../about';
