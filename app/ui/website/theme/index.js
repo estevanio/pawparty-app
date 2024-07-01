a65894c94ecd2b0a6b495a3922a52546abbda363
stavinopolis <estevanbenjamin@gmail.com>
2024-07-01 04:16:25 -0600

    Refactored palette

--- app/ui/website/theme/index.js
+++ app/ui/website/theme/index.js
@@ -1,12 +1,41 @@
 import { responsiveFontSizes } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import shadows from './shadows';
-import { light, dark } from './palette';
 
 const getTheme = (mode, themeToggler) =>
   responsiveFontSizes(
     createTheme({
-      palette: mode === 'light' ? light : dark,
+      palette: {
+        alternate: {
+          main: '#f7faff',
+          dark: '#edf1f7',
+        },
+        cardShadow: 'rgba(23, 70, 161, .11)',
+        mode: 'light',
+        primary: {
+          main: '#377dff',
+          light: '#467de3',
+          dark: '#2f6ad9',
+          contrastText: '#fff',
+        },
+        secondary: {
+          light: '#ffb74d',
+          main: '#f9b934',
+          dark: '#FF9800',
+          contrastText: 'rgba(0, 0, 0, 0.87)',
+        },
+        text: {
+          primary: '#1e2022',
+          secondary: '#677788',
+        },
+        divider: 'rgba(0, 0, 0, 0.12)',
+        background: {
+          paper: '#ffffff',
+          default: '#ffffff',
+          level2: '#f5f5f5',
+          level1: '#ffffff',
+        },
+      },
       shadows: shadows(mode),
       typography: {
         fontFamily: '"Inter", sans-serif',
