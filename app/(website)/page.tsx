f0bdac10e3616ac634f96d3d3a4c1be8593fdd23
jayson-ambrose <jayson.ambrose.87@gmail.com>
2025-08-05 17:37:14 -0600

    added coming soon splash

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@/app/ui/website/Container';
+import Image from 'next/image';
 
 import { default as Features } from '@/app/ui/website/Features';
 import { default as Folio } from '@/app/ui/website/Folio';
@@ -9,8 +10,13 @@ import { default as Hero } from '@/app/ui/website/Hero';
 import { default as Services } from '@/app/ui/website/Services';
 import { default as HowItWorks} from '@/app/ui/website/HowItWorks';
 import { default as Sponsors } from '@/app/ui/website/Sponsors';
+import { Typography } from '@mui/material';
+
+const siteLive = false
 
 const Page = () => (
+
+  siteLive ? 
   <>
     <Container id='herosection'>
       <Hero />
@@ -37,7 +43,16 @@ const Page = () => (
     <Container id='sponsorsection'>
       <Sponsors/>
     </Container>
+  </> :
+  <>
+  <Container>
+    <Box sx={{ textAlign: 'center', padding: 1, backgroundColor: 'primary.main', rounded: '8px' }}> 
+      <Image src="pawparty-logo.svg" alt="Logo" width={400} height={400} />
+    </Box>
+  <Typography sx={{textAlign: 'center', fontSize: 24, fontFamily: 'Montserrat', fontWeight: 'bold'}} >Site coming soon!</Typography>
+  </Container>
   </>
+  
 );
 
 export default Page;
